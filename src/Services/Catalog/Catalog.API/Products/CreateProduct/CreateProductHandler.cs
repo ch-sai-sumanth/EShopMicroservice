b74ce5c6f009fb29c6c93b689d3fc7259dac6d1c
ch-sai-sumanth <saisumanth0531@gmail.com>
2025-10-20 00:53:25 +0530

    Implement product creation endpoint with MediatR and Carter integration

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand,CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //TODO: Save to database
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,19 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
+app.MapCarter();
+app.UseHttpsRedirection();
 app.Run();
