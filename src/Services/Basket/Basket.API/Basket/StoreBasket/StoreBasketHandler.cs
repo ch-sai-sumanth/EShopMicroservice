35a2a7987358dde82a5c18c7e5dbd648ea1caf58
ch-sai-sumanth <saisumanth0531@gmail.com>
2025-10-29 14:43:50 +0530

    Add Basket.API as GRPC Client

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -12,24 +14,24 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 public class StoreBasketHandler
-    (IBasketRepository repository): ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto): ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        // await DeductDiscounts(command.Cart, cancellationToken);
+        await DeductDiscounts(command.Cart, cancellationToken);
 
         await repository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketResult(command.Cart.UserName);
     }
 
 
-    // private async Task DeductDiscounts(ShoppingCart cart,CancellationToken cancellationToken)
-    // {
-    //     // Communicate with Discount.Grpc and calculate lastest prices of products into sc
-    //     foreach (var item in cart.Items)
-    //     {
-    //         var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
-    //         item.Price -= coupon.Amount;
-    //     }
-    // }
+    private async Task DeductDiscounts(ShoppingCart cart,CancellationToken cancellationToken)
+    {
+        // Communicate with Discount.Grpc and calculate lastest prices of products into shopping cart
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+    }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,6 @@
 
 using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -28,6 +29,23 @@ builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+    {
+        options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+    })
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        return handler;
+    });
+
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
